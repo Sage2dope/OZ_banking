31536137266be198b6f1cb0f8903a72f83f9dad6
sage2dope <popezamani@gmail.com>
2025-01-05 20:29:42 +0000

    update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -200,14 +200,14 @@ export const authFormSchema = (type: string) => z.object({
   email: z.string().email(),
   password: z.string().min(8),
   //sign-up
-  firstName: type === 'sign-in' ? z.string().optional() : z.string(),
-  lastName: type === 'sign-in' ? z.string().optional() : z.string(),
-  address1: type === 'sign-in' ? z.string().optional() : z.string(),
-  city: type === 'sign-in' ? z.string().optional() : z.string(),
-  state: type === 'sign-in' ? z.string().optional() : z.string(),
-  postalCode: type === 'sign-in' ? z.string().optional() : z.string(),
-  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string(),
-  ssn: type === 'sign-in' ? z.string().optional() : z.string(),
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(20),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
 
   
 });
